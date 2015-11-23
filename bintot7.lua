eca31eac2ff52e1cb3ab66520371786a791c5cf8
Michael Rotman <rotmanmi@post.tau.ac.il>
2015-11-23 10:29:06 +0200

    Changed normalization so analogy can be used inside distance

--- bintot7.lua
+++ bintot7.lua
@@ -1,7 +1,6 @@
 file = torch.DiskFile(opt.binfilename,'r')
 local max_w = 50
 
-
 function readStringv2(file)  
 	local str = {}
 	for i = 1,max_w do
@@ -26,7 +25,6 @@ file:ascii()
 words = file:readInt()
 size = file:readInt()
 
-
 local w2vvocab = {}
 local v2wvocab = {}
 local M = torch.FloatTensor(words,size)
@@ -37,12 +35,16 @@ for i = 1,words do
 	local str = readStringv2(file)
 	local vecrep = file:readFloat(300)
 	vecrep = torch.FloatTensor(vecrep)
-	local norm = torch.norm(vecrep,2)
-	if norm ~= 0 then vecrep:div(norm) end
+
+--	local norm = torch.norm(vecrep,2)
+--	if norm ~= 0 then vecrep:div(norm) end
 	w2vvocab[str] = i
 	v2wvocab[i] = str
 	M[{{i},{}}] = vecrep
+
 end
+
+local norms = torch.norm(M,2,2)
 
 
 --Writing Files
@@ -50,7 +52,9 @@ word2vec = {}
 word2vec.M = M
 word2vec.w2vvocab = w2vvocab
 word2vec.v2wvocab = v2wvocab
+word2vec.norms = norms
 torch.save(opt.outfilename,word2vec)
+
 print('Writing t7 File for future usage.')
 
 
--- w2vutils.lua
+++ w2vutils.lua
@@ -6,7 +6,7 @@ opt = {
 }
 local w2vutils = {}
 if not paths.filep(opt.outfilename) then
-	w2vutils = require('bintot7.lua')
+	w2vutils = require('bintot7')
 else
 	w2vutils = torch.load(opt.outfilename)
 	print('Done reading word2vec data.')
@@ -18,7 +18,7 @@ w2vutils.distance = function (self,vec,k)
 	--self.zeros = self.zeros or torch.zeros(self.M:size(1));
 	local norm = vec:norm(2)
 	vec:div(norm)
-	local distances = torch.mv(self.M ,vec)
+	local distances = torch.cdiv(self.M,torch.expand(self.norms,self.norms:size(1),self.M:size(2))) * vec
 	distances , oldindex = torch.sort(distances,1,true)
 	local returnwords = {}
 	local returndistances = {}
@@ -33,7 +33,7 @@ w2vutils.word2vec = function (self,word,throwerror)
    local throwerror = throwerror or false
    if word == 'of' then word = 'Of' end
    local ind = self.w2vvocab[word]
-   if ind == nil then return torch.Tensor(300):fill(0)
+   if ind == nil then return torch.Tensor(300):fill(0) end
    if throwerror then
 		assert(ind ~= nil, 'Word does not exist in the dictionary!')
    end
